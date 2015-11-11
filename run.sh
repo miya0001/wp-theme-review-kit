b5755184765810dbe38e26616bc98fb650e26e1b
miya0001 <miya@wpist.me>
2015-11-11 23:30:49 +0900

    some fix

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 set -ex;
 
 curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash
+curl -L -o provision-post https://raw.githubusercontent.com/miya0001/wp-theme-review-kit/master/provision-post.sh
