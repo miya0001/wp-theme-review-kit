de55844a7ce32755a151980310a323b8dae2b89a
miya0001 <miya@wpist.me>
2015-11-11 23:38:58 +0900

    some fix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -ex;
 
 curl -o provision-post https://raw.githubusercontent.com/miya0001/wp-theme-review-kit/master/provision-post.sh
-curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash -s wptheme
+curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash -s root "" wptheme
