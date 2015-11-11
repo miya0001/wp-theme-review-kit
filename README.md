a9348524340850b69a7f307d51321824c58f09ae
miya0001 <miya@wpist.me>
2015-11-11 23:37:22 +0900

    some fix

---
+++ README.md
@@ -0,0 +1,5 @@
+# WordPress Theme Review Kit
+
+```
+$ curl https://raw.githubusercontent.com/miya0001/wp-theme-review-kit/master/run.sh | bash
+```
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -ex;
 
-curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash
-curl -L -o provision-post https://raw.githubusercontent.com/miya0001/wp-theme-review-kit/master/provision-post.sh
+curl -o provision-post https://raw.githubusercontent.com/miya0001/wp-theme-review-kit/master/provision-post.sh
+curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash -s wptheme
