a7781a78150fe2e2e21eba8151c242060882ca7c
miya0001 <miya@wpist.me>
2015-12-06 12:06:36 -0500

    add some args

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 
 set -ex;
 
+DB_USER=${1-root}
+DB_PASS=$2
+DB_NAME=${3-wptheme}
+PORT=8080
+
 curl -o provision-post.sh https://raw.githubusercontent.com/miya0001/wp-theme-review-kit/master/provision-post.sh
-curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash -s root "" wptheme
+curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash -s "$DB_USER" "$DB_PASS" "$DB_NAME"
